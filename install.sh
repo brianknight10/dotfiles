1fd3c65b5c05c817e5252247f080ca91612f21a8
Brian Knight <brianknight10@gmail.com>
2015-09-02 15:02:50 -0400

    Create git configuration files.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get the current directory of the script
+
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Create symlinks
+
+#ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+#ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
