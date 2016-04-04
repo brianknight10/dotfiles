439188394735fb16c45550c7a2e07d7cda40ed37
Brian Knight <brianknight10@gmail.com>
2016-04-04 16:33:50 -0400

    Delete .gcloud.bash

--- system/.gcloud.bash
+++
@@ -1,5 +0,0 @@
-# Enable shell command completion for gcloud
-source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
-
-# Add Google Cloud SDK command line to $PATH
-source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
