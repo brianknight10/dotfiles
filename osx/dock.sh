4867052b808cf5967bedc9863df2dd78ecceb3c8
Brian Knight <brianknight10@gmail.com>
2016-04-04 16:37:55 -0400

    Update dock.sh

--- osx/dock.sh
+++ osx/dock.sh
@@ -4,9 +4,9 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/iterm2/2.1.4/iTerm.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/sublime-text3/Build 3083/Sublime Text.app"
+dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/sublime-text3/3103/Sublime Text.app"
 dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
-dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/slack/1.1.3/Slack.app"
-dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/evernote/6.1_452115/Evernote.app"
+dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/slack/2.0.1/Slack.app"
+dockutil --no-restart --add "/opt/homebrew-cask/Caskroom/evernote/6.6_453293/Evernote.app"
 
 killall Dock
