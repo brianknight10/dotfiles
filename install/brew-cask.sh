31de1794850aebfd4a2b986a1fe0a1b5be248d13
Brian Knight <brianknight10@gmail.com>
2016-04-04 16:13:03 -0400

    Update brew-cask.sh

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -3,15 +3,8 @@ apps=(
     dockertoolbox
     dropbox
     evernote
-    gas-mask
-    google-chrome
-    google-cloud-sdk
     iterm2
     java
-    mysqlworkbench
-    pgadmin3
-    screenhero
-    skype
     slack
     sublime-text3
     terraform
