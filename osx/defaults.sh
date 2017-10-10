46767043a054a41188a4ff56d05501aaeca09dde
Brian Knight <brianknight10@gmail.com>
2017-10-10 13:57:55 -0400

    Update defaults.sh

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -9,10 +9,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "bouncex"
-sudo scutil --set HostName "bouncex"
-sudo scutil --set LocalHostName "bouncex"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "bouncex"
+sudo scutil --set ComputerName "knightx"
+sudo scutil --set HostName "knightx"
+sudo scutil --set LocalHostName "knightx"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "knightx"
 
 # Set standby delay to 24 hours (default is 1 hour)
 sudo pmset -a standbydelay 86400
